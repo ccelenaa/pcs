cd0a09ba77bbdc7d96212754dc43e05dfd3e0731
Celina Chouali <celinachouali47@gmail.com>
2024-05-23 19:21:19 +0200

    Fix data

--- docker/postgres/init-db/2-data.sql
+++ docker/postgres/init-db/2-data.sql
@@ -12,3 +12,32 @@ INSERT INTO public.prestataire (name,email,login,"password",locale,"data",verifi
 INSERT INTO public.voyageur (name,email,tel,login,"password",locale,"data",verified_at,suspended_at,created_at,updated_at,deleted_at) VALUES
 	 ('Pierre Voyageur1','pcs.voyageur1@gmail.com','0643145245','voyageur1','$argon2id$v=19$m=65536,t=3,p=4$bEjs89VfnTwWKRccB1FIzA$tJ+OX5IyTxwBuLYTTYGodt1QPw1TUBasZ77qVphAo0g','fr','{}',NULL,NULL,'2024-05-16 02:20:52.817','2024-05-16 02:20:52.817',NULL),
 	 ('Manuel Voyageur2','pcs.voyageur2@gmail.com','0765542543','voyageur2','$argon2id$v=19$m=65536,t=3,p=4$HYRLvK/aOxLXPXsiJ0JSqg$BRa9LNYwX/u926zUM08yKgR1Qvi7VMpoS3iQ2fEQlwI','fr','{}',NULL,NULL,'2024-05-16 02:20:52.822','2024-05-16 02:20:52.822',NULL);
+INSERT INTO public.voyageur (name,email,tel,login,"password",locale,"data",verified_at,suspended_at,created_at,updated_at,deleted_at) VALUES
+	 ('Pierre Voyageur1','pcs.voyageur1@gmail.com','0643145245','voyageur1','$argon2id$v=19$m=65536,t=3,p=4$bEjs89VfnTwWKRccB1FIzA$tJ+OX5IyTxwBuLYTTYGodt1QPw1TUBasZ77qVphAo0g','fr','{}',NULL,NULL,'2024-05-16 02:20:52.817','2024-05-16 02:20:52.817',NULL),
+	 ('Manuel Voyageur2','pcs.voyageur2@gmail.com','0765542543','voyageur2','$argon2id$v=19$m=65536,t=3,p=4$HYRLvK/aOxLXPXsiJ0JSqg$BRa9LNYwX/u926zUM08yKgR1Qvi7VMpoS3iQ2fEQlwI','fr','{}',NULL,NULL,'2024-05-16 02:20:52.822','2024-05-16 02:20:52.822',NULL);
+INSERT INTO public.type_prestation (label) VALUES
+	 ('Remise des clés'),
+	 ('Taxieur'),
+	 ('Deplacement de bagages'),
+	 ('Cuisine a domicile'),
+	 ('Nettoyages'),
+	 ('Coach sportif');
+INSERT INTO public.prestataire_type_prestation (id_prestataire,id_type_prestation,price) VALUES
+	 (1,1,30),
+	 (1,3,50),
+	 (1,4,100),
+	 (1,5,70),
+	 (1,6,150),
+	 (2,1,30),
+	 (2,2,100),
+	 (2,3,60),
+	 (2,4,80),
+	 (2,5,80);
+INSERT INTO public.bien (id_bailleur,type,area,description,status,price,currency) VALUES
+	 (1,'appartement','62m2','Appartement en plein Paris','disponible',150,'€'),
+	 (1,'appartement','55m2','Appartement en Seine-et-Marne','disponible',90,'€'),
+	 (1,'studio','32m2','Studio en plein Paris','disponible',80,'€'),
+	 (1,'appartement','62m2','Appartement a Lyon','disponible',100,'€'),
+	 (2,'appartement','80m2','Appartement a coté de Paris','disponible',120,'€'),
+	 (2,'appartement','55m2','Appartement a la defense','disponible',110,'€'),
+	 (2,'studio','27m2','Studio a Evry','disponible',60,'€');
